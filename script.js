f51141541562e631150d51c630bd615f02d1847b
London Bielicke <london.bielicke1313@gmail.com>
2022-06-03 15:04:44 -0500

    first update

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ var updates = {
   "Week One": "Before heading to Delaware Dr. Mauriello and I discussed the research project that I will be working with along with the tools and frameworks that we will be using for my part of the research project. My first task was to make an executable using the .NET framework that will track user keystrokes along with the timing of those keystrokes and also certain tasks that the user has open in task manager like how may tabs they are using in Chrome. The goal with this data is to test this with the facial recognition software to see if we can get better results about when a user becomes stressed. Because I already knew C#, the .NET framework was easy to navigate and I coded a simple command line application before getting to the research lab. My next steps will be better managing and storing the data."
 };
 
+function displayUpdate(key) {
+  var curr =  document.getElementById("stuff");
+  var next = document.getElementById("update");
+  next.innerHTML = "<h1> " + key + " </h1>";
+  next.innerHTML += updates[key];
+  curr.style.display="none";
+  next.style.display = "inline"
+}
+
+function displayMain() {
+  var curr =  document.getElementById("update");
+  var next = document.getElementById("stuff");
+  curr.style.display="none";
+  next.style.display = "inline"
+}
+
 function generateTable() {
   var table = document.getElementById("weeks");
   var row_count = 0;
@@ -14,6 +30,9 @@ function generateTable() {
     }
       var td = document.createElement("td");
       td.innerHTML = key;
+      td.addEventListener("click", function(){
+          displayUpdate(key);
+      }, false); //where func is your function name
        tr.appendChild(td);
       if (row_count == 0) {
          table.appendChild(tr);
